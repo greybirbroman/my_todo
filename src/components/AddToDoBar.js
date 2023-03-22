355436be27c65261cf3742bcdfceecdc61b41c90
greybirbroman <98167513+greybirbroman@users.noreply.github.com>
2023-03-22 12:35:52 +0300

    initial, add tailwind config

---
+++ src/components/AddToDoBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AddToDoBar() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ['./src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      screens: {
+        'sm': {'min': '320px', 'max': '767px'},
+        // => @media (min-width: 320px and max-width: 767px) { ... }
+  
+        'md': {'min': '768px', 'max': '1023px'},
+        // => @media (min-width: 768px and max-width: 1023px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+      },
+    },
+  },
+  plugins: [],
+}
